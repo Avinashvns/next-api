7f52f6768c224872ae81c01b26348933062f511f
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-02-15 20:11:59 +0530

    Create A Static Db files

---
+++ src/util/db.js
@@ -0,0 +1,33 @@
+export const user=[
+    {
+        name : "avi",
+        age: 30,
+        email : "avi@gmail.com"
+    },
+    {
+        name: "rahul",
+        age: 25,
+        email: "rahul@example.com"
+    },
+    {
+        name: "sneha",
+        age: 28,
+        email: "sneha@example.com"
+    },
+    {
+        name: "vikas",
+        age: 32,
+        email: "vikas@example.com"
+    },
+    {
+        name: "priya",
+        age: 27,
+        email: "priya@example.com"
+    },
+    {
+        name: "arjun",
+        age: 29,
+        email: "arjun@example.com"
+    }
+    
+]
