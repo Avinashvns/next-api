f880481b38f53f9faab0873df8e1a7c5f512aefc
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-02-16 11:54:27 +0530

    PUT API Create Backend Route

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -9,3 +9,19 @@ export function GET(request, content) {
     userData.length==0?{result:`No Data Found ID: ${content.params.id}`} : {result : userData[0], success:true},
     {status: 200})
 }
+
+export async function PUT(request,content){
+    let payload = await request.json()
+    payload.id = content.params.id
+    console.log(payload.id)
+
+    let missing = [];
+    if (!payload.name) missing.push("name");
+    if (!payload.age) missing.push("age");
+    if (!payload.email) missing.push("email");
+
+    if(missing.length){
+        return NextResponse.json({result : `Missing: ${missing.join(", ")}` , success: false},{status: 400})
+    }
+    return NextResponse.json({result:payload, success: true}, {status: 200})
+}
