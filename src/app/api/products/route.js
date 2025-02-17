fc487c4a994ecef7220ac3ade3b2593e05df0540
AVINASH SINGH <83054042+Avinashvns@users.noreply.github.com>
2025-02-17 19:55:36 +0530

    POST API In MONGOOSE

--- src/app/api/products/route.js
+++ src/app/api/products/route.js
@@ -10,5 +10,16 @@ export async function GET(){
 
     const data = await Product.find()
     console.log("Data : " , data)
-    return NextResponse.json({result : true, data})
+    return NextResponse.json({success : true, data})
+}
+
+export async function POST(){
+    await connectDB()
+    let newData = new Product({
+        name : "ram",
+        age : 30000,
+        email : "god@gmail.com"
+    })
+    const result = await newData.save()
+    return NextResponse.json({success: true, result})
 }
